cadbbc6c511b3e35269e42a5d0106995b69378e3
sarontew <98852405+sarontew@users.noreply.github.com>
2026-02-24 19:43:19 +0000

    Use Mask-RCNN to pool features directly from masks

--- VOST/gnn_experiment.py
+++ VOST/gnn_experiment.py
@@ -10,53 +10,97 @@ from scipy import ndimage
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from utils import save_region_features
+from torchvision import transforms, models
+from torchvision.models import resnet50
 from torch_geometric.nn import GCNConv, global_mean_pool
 from torch_geometric.data import Data
+from cnn_baseline import CNN_Classifier
 from torch_geometric.loader import DataLoader
 from skimage.measure import label, regionprops
+from torchvision.models.detection.roi_heads import roi_align
 
 # video = "0_squeeze_cloth"
-video = "7_squeeze_pasta"
+video = "1214_cut_mellon"
 frame_count = 0
 subimages_per_frame = {} # frame_number : [subimages]
 
+resnet = models.resnet50(pretrained=True)
+backbone = torch.nn.Sequential(*list(resnet.children())[:-2])
+backbone.eval()
+
+# @profile
 for image_name in os.listdir(f"JPEGImages/{video}"):
-    frame = cv2.imread(f"JPEGImages/{video}/{image_name}")
     subimages_per_frame[frame_count] = []
-    mask_name = image_name[0:-3] + "png"
+    
+    if frame_count != -1:
 
-    mask_parent_dir = "Annotations"
-    mask = cv2.imread(f"{mask_parent_dir}/{video}/{mask_name}", cv2.IMREAD_GRAYSCALE)
-    mask_gray = mask[:, :, 0] if len(mask.shape) == 3 else mask
-
-    # Robust threshold: anything > 0 is ROI
-    binary_mask = np.where(mask_gray > 0, 255, 0).astype(np.uint8)
-    roi = cv2.bitwise_and(frame, frame, mask=binary_mask)
-
-    labeled = label(binary_mask, connectivity=2) # 2 = 8 connectivity, type:ndarray
-    regions = regionprops(labeled) # type: list
-
-    # Get each region's sub-image from masked out
-    for r in regions:
-        min_row, min_col, max_row, max_col = r.bbox
-        subimage = roi[min_row:max_row, min_col:max_col]
-        # cv2.imshow("subimage", subimage)
+        frame = cv2.imread(f"JPEGImages/{video}/{image_name}")
+        print("frame")
+        # cv2.imshow("frame", frame)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
-        # print("subimage type is", type(subimage))
-        # print("with shape", subimage.shape)
+        
+        mask_name = image_name[0:-3] + "png"
 
-        subimages_per_frame[frame_count].append(subimage) # each chunk from each frame
-    
-    # plt.imshow(labeled, cmap="tab20")
-    # plt.colorbar()
-    # plt.show()
+        mask_parent_dir = "Annotations"
+        mask = cv2.imread(f"{mask_parent_dir}/{video}/{mask_name}", cv2.IMREAD_GRAYSCALE)
+        mask_gray = mask[:, :, 0] if len(mask.shape) == 3 else mask
 
-    frame_count += 1
+        # Robust threshold: anything > 0 is ROI
+        binary_mask = np.where(mask_gray > 0, 255, 0).astype(np.uint8)
+        roi = cv2.bitwise_and(frame, frame, mask=binary_mask)
+
+        labeled = label(binary_mask, connectivity=2) # 2 = 8 connectivity, type:ndarray
+        regions = regionprops(labeled) # type: list
+
+        # Get each region's sub-image from masked out
+        for r in regions:
+            min_row, min_col, max_row, max_col = r.bbox
+            subimage = roi[min_row:max_row, min_col:max_col]
+            # cv2.imshow("subimage", subimage)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            # print("subimage type is", type(subimage))
+            # print("with shape", subimage.shape)
+
+            #resnet = models.resnet50(pretrained=True)
+            # Remove avgpool and fc
+            #backbone = torch.nn.Sequential(*list(resnet.children())[:-2])
+            #x = torch.randn(1, 3, 224, 224)
+            #feature_map = backbone(x)
+
+            frame_tensor = torch.from_numpy(frame).permute(2,0,1).unsqueeze(0).float()
+            with torch.no_grad():
+                backbone_features = backbone(frame_tensor)
+                bbox = torch.tensor([[0, min_col, min_row, max_col, max_row]], dtype=torch.float32)
+                
+                pooled = roi_align(input=backbone_features, boxes=bbox, output_size=(7,7)) # 1, 2048, 7, 7
+                pooled = pooled.squeeze(0) # remove the first dim
+
+                #subimages_per_frame[frame_count].append(subimage) # each chunk from each frame
+                # add the pooled features instead
+                subimages_per_frame[frame_count].append(pooled.cpu())
+
+        
+        # plt.imshow(labeled, cmap="tab20")
+        # plt.colorbar()
+        # plt.show()
+
+        frame_count += 1
+        print(frame_count)
+
+print("done extracting feature for each subimage of interest")
 
 # Graph creation
 # Node: each sub-images from every single frame
 # Edge: connecting subimages from frame i to every subimage from frame i+1
+
+# print("before creating edges, checking the node features i have saved")
+# for subimage in subimages_per_frame.items():
+#     print(len(subimage))
+#     for s in subimage:
+#         print("s is ",type(s))
+
 
 edges_connecting_from = []
 edges_connecting_to = []
@@ -90,77 +134,70 @@ for idx, frame_id in enumerate(frame_ids):
             edges_connecting_from.append(source_node)
             edges_connecting_to.append(target_node)
 
-# Not all subimages are the same size - pad
-# for x in sub_images_as_nodes:
-#     print("shape of each is", x.shape)
-# original_shapes = [img.shape for img in sub_images_as_nodes]
-# original_lengths = [img.size for img in sub_images_as_nodes]
+# Hs = [img.shape[0] for img in sub_images_as_nodes]
+# Ws = [img.shape[1] for img in sub_images_as_nodes]
+# C  = sub_images_as_nodes[0].shape[2]
 
-# flattened_nodes = [node.reshape(-1) for node in sub_images_as_nodes]
-# # for x in flattened_nodes:
-# #     print("after flattening shape of each is", x.shape)
-# max_len = max(node.shape[0] for node in flattened_nodes)
-# print("max len is", max_len)
-# padded_nodes = np.stack([
-#     np.pad(node, (0, max_len - node.shape[0]))
-#     for node in flattened_nodes
-# ])
-# for x in padded_nodes:
-#     print("after padding", x.shape)
-# nodes = torch.tensor(padded_nodes, dtype=torch.float)
+# max_H = max(Hs)
+# max_W = max(Ws)
 
-# restored_images = []
-# for i, (shape, length) in enumerate(zip(original_shapes, original_lengths)):
-#     unpadded = nodes[i, :length]
-#     restored = unpadded.reshape(shape)   # (H, W, C)
-#     restored_images.append(restored)
-#     print(restored.shape)
+# padded_images = []
 
-# assume sub_images_as_nodes is a list of (H, W, C) arrays
+# for img in sub_images_as_nodes:
+#     assert img.ndim == 3, img.shape  # (H, W, C)
+#     H, W, C = img.shape
+#     print("shape")
+#     print(H,W,C)
 
-Hs = [img.shape[0] for img in sub_images_as_nodes]
-Ws = [img.shape[1] for img in sub_images_as_nodes]
-C  = sub_images_as_nodes[0].shape[2]
+#     cv2.imshow("subimage featurez", img)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
 
-max_H = max(Hs)
-max_W = max(Ws)
+#     pad_h = max_H - H
+#     pad_w = max_W - W
 
-padded_images = []
+#     # pad format: ((top,bottom), (left,right), (channels))
+#     padded = np.pad(
+#         img,
+#         pad_width=((0, pad_h), (0, pad_w), (0, 0)),
+#         mode="constant",  # or "edge", "reflect", etc.
+#         constant_values=0
+#     )
+#     # print("padded shape", padded.shape)
+#     # cv2.imshow("subimage", padded)
+#     # cv2.waitKey(0)
+#     # cv2.destroyAllWindows()
+#     assert padded.shape == (max_H, max_W, C)
+#     padded_images.append(padded)
 
-for img in sub_images_as_nodes:
-    assert img.ndim == 3, img.shape  # (H, W, C)
-    H, W, C = img.shape
-    # print("shape")
-    # print(H,W,C)
+#batch = torch.from_numpy(np.stack(padded_images))
 
-    # cv2.imshow("subimage", img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+#FEATURE BASED BATCH
+# print("now after creating edges, before creating batch")
+# for k, subimage in subimages_per_frame.items():
+#     print(len(subimage))
+#     for s in subimage:
+#         print(type(s), k)
+        # if type(s) == list:
+        #     print("getting list type for key", k)
+        # elif type(s) == int:
+        #     print("getting CORRECT int type for key", k)
 
-    pad_h = max_H - H
-    pad_w = max_W - W
+# print(type(subimages_per_frame.values()))
+# nodes = torch.from_numpy(np.stack(subimages_per_frame.values()))
 
-    # pad format: ((top,bottom), (left,right), (channels))
-    padded = np.pad(
-        img,
-        pad_width=((0, pad_h), (0, pad_w), (0, 0)),
-        mode="constant",  # or "edge", "reflect", etc.
-        constant_values=0
-    )
-    # print("padded shape", padded.shape)
-    # cv2.imshow("subimage", padded)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    assert padded.shape == (max_H, max_W, C)
-    padded_images.append(padded)
+# NEW: flatten to make everything even - NOT SURE
+all_nodes = []
+for frame_id in subimages_per_frame:
+    for pooled in subimages_per_frame[frame_id]:
+        all_nodes.append(pooled)
+nodes = torch.stack(all_nodes) 
 
-batch = torch.from_numpy(np.stack(padded_images))
-
-video_1_graph = Data(x=batch, edge_index=torch.tensor([edges_connecting_from, edges_connecting_to], dtype=torch.long))
-# g = torch_geometric.utils.to_networkx(video_1_graph, to_undirected=True)
-# plt.figure()
-# nx.draw(g)
-# plt.show()
+video_1_graph = Data(x=nodes, edge_index=torch.tensor([edges_connecting_from, edges_connecting_to], dtype=torch.long))
+g = torch_geometric.utils.to_networkx(video_1_graph, to_undirected=True)
+plt.figure()
+nx.draw(g)
+plt.show()
 
 # x: node feature matrix with shape [num_nodes, num_node_features]
 # edge_index: graph connectivity in COO format with shape [2, num_edges]
@@ -178,6 +215,13 @@ video_1_graph.validate(raise_on_error=True)
 class St_GCN_Classifier(torch.nn.Module):
     def __init__(self, num_classes = 10):
         super().__init__()
+
+        # model = resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
+        # for name, layer in model.named_children():
+        #     if name in ['conv1', 'bn1', 'layer1', 'layer2']:
+        #         for param in layer.parameters():
+        #             param.requires_grad = False
+            
 
         self.hidden_channels = 64
         self.conv1 = GCNConv(2048,self.hidden_channels) # input feature per node = 2, output feature per node =4
@@ -198,34 +242,46 @@ class St_GCN_Classifier(torch.nn.Module):
         return x
 
 
-dataset = [video_1_graph]
+dataset = [video_1_graph] # graph of images not features
+cnn_dataset = []
 train_loader = DataLoader(dataset, batch_size=64, shuffle = True)
 
 # trying to extract the features of each node
-save_region_features(train_loader)
+save_region_features(train_loader) # i dont think its actually saving anything
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = St_GCN_Classifier(num_classes=2).to(device)
-optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
+gnn_model = St_GCN_Classifier(num_classes=2).to(device)
+cnn_model = CNN_Classifier(num_classes=1).to(device) # TODO: fix num_classes, get unique object/action labels based on videos
+gnn_optimiser = torch.optim.Adam(gnn_model.parameters(), lr=0.001)
+cnn_optimiser = torch.optim.Adam(gnn_model.parameters(), lr=0.001)
 criterion = torch.nn.CrossEntropyLoss()
 
-model.train()
+gnn_model.train()
+cnn_model.train()
 for epoch in range(50):
     total_loss = 0
 
-    for batch in train_loader:
+    for batch in train_loader: # for frame, graph
         batch = batch.to(device)
 
-        optimiser.zero_grad()
-        output = model(batch.x, batch.edge_index, batch.batch)
-        loss = criterion(output, batch.y)
-        loss.backwards()
-        optimiser.step()
+        gnn_optimiser.zero_grad()
+        #cnn_optimiser.zero_grad()
 
-        total_loss += loss.item()
+        #cnn_pred = cnn_model() # input the frame
+        gnn_pred = gnn_model(batch.x, batch.edge_index, batch.batch)
 
-    print(f"Epoch {epoch:03d}, Loss: {total_loss:.4f}")
+        gnn_loss = criterion(gnn_pred, batch.y)
+        #cnn_loss = criterion(cnn_pred, batch.y)
+        
+        #all_cnn_loss_per_videos += cnn_loss
+        
+        gnn_loss.backward()
+        gnn_optimiser.step()
+
+        total_gnn_loss += gnn_loss.item()
+
+    print(f"Epoch {epoch:03d}, GNN Loss: {total_gnn_loss:.4f}")
 
 
 # model = MyGCN()
