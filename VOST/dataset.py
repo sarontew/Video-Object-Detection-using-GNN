63bda6ec61803f97509112bd3a65427a25a0b60c
sarontew <98852405+sarontew@users.noreply.github.com>
2026-03-02 11:31:48 +0000

    Add kfold validation, unqiue object id per video for labels

--- VOST/dataset.py
+++ VOST/dataset.py
@@ -5,8 +5,12 @@ import numpy as np
 import os
 import cv2
 
+def extract_label_from_filename(filename):
+    # Split by underscore, take last part before extension
+    return filename.split('_')[-1].split('.')[0]  # 'cloth' in your example
+
 class VODDataset(Dataset):
-    def __init__(self, video_names, frames= (None,None), task='object_rec', split='train'):
+    def __init__(self, video_names, unique_label_mappings, frames= (None,None), task='object_rec', split='train'):
         """
         :param video_names: Video names to extract frames from
         :param frames: Range of frames to read
@@ -21,9 +25,11 @@ class VODDataset(Dataset):
         self.original_label = []
         self.unique_label_mappings = {}
         self.split = split
+        self.unique_labels_map = unique_label_mappings 
         
         for video in self.video_names:
             parent_path = f"JPEGImages/{video}"
+            print("extracting frames for video", video)
             all_frames = []
             for name in os.listdir(parent_path):
                 all_frames.append(name)
@@ -65,6 +71,9 @@ class VODDataset(Dataset):
                         self.original_label.append(target_name)
                         if target_name not in self.unique_label_mappings.keys(): # Ensure unique id for each object
                             self.unique_label_mappings[target_name] = self.video_names.index(video)
+                            # print("target name is", target_name)
+                            # print("unique label id", self.video_names.index(video))
+                            # print("video id is", video)
                 else:
                     print("Extracting for all frames of a video")
        
@@ -75,6 +84,15 @@ class VODDataset(Dataset):
         img = self.frames[idx]
         img = torch.FloatTensor(self.frames[idx]).permute(2,0,1).float() if img.ndim == 3 else torch.FloatTensor(self.frames[idx]).permute(0,3,1,2).float()
         object_label = self.original_label[idx] # e.g. butter
-        object_class_id = self.unique_label_mappings[object_label] # id corresponding to object e.g. 2
+        # object_class_id = self.unique_label_mappings[object_label] # id corresponding to object e.g. 2
+        # labels = torch.tensor(object_class_id)
+        # return img, labels
+    
+        # Extract label from filename
+        #filename = self.video_names[idx]
+        #object_label = extract_label_from_filename(filename)
+        
+        object_class_id = self.unique_labels_map[object_label]
         labels = torch.tensor(object_class_id)
+        
         return img, labels
--- VOST/main.py
+++ VOST/main.py
@@ -12,22 +12,27 @@ from torchvision.models.feature_extraction import get_graph_node_names
 from dataset import VODDataset
 from torch.utils.data import Dataset, TensorDataset, DataLoader
 from cnn_baseline import CNN_Classifier
+from sklearn.model_selection import KFold
 from utils import get_file_names, get_unique_labels, save_features
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
 ## TRAIN
-def train(train_loader, model, optimiser, lossfn):
+def train(train_loader, model, optimiser, lossfn, fold):
+    #for epoch in range(20):
     model.train()
+    running_loss = 0.0
     for img, target in train_loader:
         images = img.to(device) # Video feature input with dim 1x2028
         optimiser.zero_grad()
         pred = model(images)
-        # predicted_class = torch.argmax(pred, dim=1)
         loss = lossfn(pred, target)
         loss.backward()
         optimiser.step()
+        running_loss += loss.item()
+
+        # print(f"Fold {fold+1}, Epoch {epoch+1}, Train Loss: {running_loss/len(train_loader)}")
 
     print("final loss is ",  loss)
 
@@ -76,6 +81,10 @@ def test(model, test_loader, lossfn, device):
 
     print(f"Frame Accuracy: {100*frame_acc:.1f}%, Video Accuracy: {100*video_acc:.1f}%, Loss: {test_loss:.6f}")
 
+def extract_label_from_filename(filename):
+    # Split by underscore, take last part before extension
+    return filename.split('_')[-1].split('.')[0]  # 'cloth' in your example
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser("Data Parser")
@@ -83,29 +92,35 @@ if __name__ == '__main__':
                         required=False)
     args = parser.parse_args()
     extract_features = bool(args.ExtractFeatures)
-    
-    train_files =  get_file_names('train.txt')
-    test_files = get_file_names('test.txt')
-    val_files = get_file_names('val.txt')
 
-    training_data_size = 9
-    testing_data_size =  5
-    validation_data_size = 5
+    all_valid_file_names = get_file_names('train.txt') + get_file_names('val.txt')
+    all_valid_file_names = all_valid_file_names[0:20]
+
+    training_data_size = int(0.8 * len(all_valid_file_names))
+    train_files = all_valid_file_names[0:training_data_size]
+    test_files = all_valid_file_names[training_data_size:]
+
     train_feature_file_name = f"resnet50_train_features.pt" # 30_ for 30 videos
     test_feature_file_name =  f"resnest50_test_features.pt"
     training_frame_range = (0,5)
     testing_frame_range = (5,8)
 
-    train_files = train_files[0:training_data_size]
-    val_files = val_files[0:validation_data_size]
+    all_files = train_files + test_files
 
-    #print("count of total unique labels is", len(get_unique_labels(train_files, val_files)))
+    # Extract all unique object names
+    all_labels = sorted({extract_label_from_filename(f) for f in all_files})
+
+    # Create consistent label → ID mapping
+    unique_label_mappings = {label: idx for idx, label in enumerate(all_labels)}
+    num_classes = len(unique_label_mappings)
+    print("Classes:", unique_label_mappings)
+    print("Total classes:", num_classes)
 
     if extract_features:
         print("Extractingg")
         saving_time_start = time.time()
-        train_dataset = VODDataset(video_names=train_files, split="train", frames=training_frame_range)
-        test_dataset = VODDataset(video_names=val_files, split="test", frames=testing_frame_range)
+        train_dataset = VODDataset(video_names=train_files, unique_label_mappings= unique_label_mappings, split="train")
+        test_dataset = VODDataset(video_names=test_files, unique_label_mappings= unique_label_mappings, split="test")
         
         train_loader = DataLoader(train_dataset, shuffle=True)
         test_loader = DataLoader(test_dataset, shuffle=False)
@@ -122,11 +137,68 @@ if __name__ == '__main__':
         train_frame = torch.load(f"Resnet50_Features/train/{train_feats_file_name}")
         all_train_features.append(train_frame['features'])
         all_train_labels.append(torch.tensor(train_frame['labels']))
-
+ 
     features = torch.cat(all_train_features, dim=0)
     labels = torch.stack(all_train_labels)
     new_train_dataset = TensorDataset(features, labels)
-    new_train_loader = DataLoader(new_train_dataset, batch_size=2, shuffle=True)
+
+    # KFold validation
+    k_folds = 5
+    num_epochs = 10
+    batch_size = 2
+
+    kfold = KFold(n_splits=k_folds, shuffle=True, random_state=42)
+    best_val_acc = 0
+    best_model_state = None
+
+    for fold, (train_ids, val_ids) in enumerate(kfold.split(new_train_dataset)):
+        print(f"\n========== Fold {fold+1}/{k_folds} ==========")
+
+        # Sample elements randomly from a given list of ids
+        train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+        val_subsampler = torch.utils.data.SubsetRandomSampler(val_ids)
+
+        # Define data loaders for training and validation
+        train_loader = DataLoader(
+            new_train_dataset,
+            batch_size=batch_size,
+            sampler=train_subsampler
+        )
+
+        val_loader = DataLoader(
+            new_train_dataset,
+            batch_size=batch_size,
+            sampler=val_subsampler
+        )
+
+        model = CNN_Classifier(
+            num_classes=num_classes # does it need test files also as possible classes?
+        ).to(device)
+
+        optimiser = torch.optim.Adam(model.parameters(), lr=1e-4)
+        lossfn = nn.CrossEntropyLoss()
+
+        train(train_loader, model, optimiser, lossfn, fold)
+
+        # Validation step
+        model.eval()
+        correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for inputs, targets in val_loader:
+                inputs, targets = inputs.to(device), targets.to(device)
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs, 1)
+
+                total += targets.size(0)
+                correct += (predicted == targets).sum().item()
+
+        val_acc = 100 * correct / total
+        print(f"Validation Accuracy: {val_acc:.2f}%")
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            best_model_state = model.state_dict()
 
     all_test_features = []
     all_test_labels = []
@@ -141,17 +213,17 @@ if __name__ == '__main__':
 
     new_test_dataset = TensorDataset(features, labels)
     new_test_loader = DataLoader(new_test_dataset, batch_size=16, shuffle=False)
-    
-    model = CNN_Classifier(num_classes=len(get_unique_labels(train_files, val_files))).to(device) # TODO: fix num_classes, get unique object/action labels based on videos
-    lossfn = nn.CrossEntropyLoss()
-    optimiser = torch.optim.AdamW(model.parameters(), lr=1e-4)
 
     training_start_time = time.time()
-    train(new_train_loader, model, optimiser, lossfn)
     training_end_time = time.time()
+
+    final_model = CNN_Classifier(
+        num_classes=num_classes
+    ).to(device)
+    final_model.load_state_dict(best_model_state) # best validation states
     
     testing_start_time = time.time()
-    test(model, new_test_loader, lossfn, device)
+    test(final_model, new_test_loader, lossfn, device)
     testing_end_time = time.time()
     
     train_time = training_end_time - training_start_time
